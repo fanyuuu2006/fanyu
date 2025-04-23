c69dd5aa4629236791cbb78c35dcaff297fe59df
Yorn90104 <bingxiao526@gmail.com>
2025-04-23 14:20:01 +0800

    first commit

---
+++ src/components/Index/IndexSection.tsx
@@ -0,0 +1,10 @@
+export const IndexSection = () => {
+  return (
+    <section>
+      <div className="container d-flex justify-center">
+        <div className="card">?</div>
+        <div className="card">?</div>
+      </div>
+    </section>
+  );
+};
