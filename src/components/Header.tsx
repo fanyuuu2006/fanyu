c68925ca02f5a3bf9e802f37c3c12b6ef25a7d3d
Yorn90104 <bingxiao526@gmail.com>
2025-04-26 01:21:24 +0800

    feature: Logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@
 import { useLanguage } from "@/context/LanguageContext";
 import { LanguageContent } from "@/types/language";
 import Link from "next/link";
+import Image from "next/image";
 
 const Routes: {
   label: LanguageContent<string>;
@@ -28,10 +29,21 @@ export const Header = () => {
   return (
     <header>
       <nav
-        className="container d-flex align-items-center"
+        className="container d-flex align-items-center justify-center"
         style={{ height: "6em", color: "white", padding: "0.5em 2em" }}
       >
-        <div className="nav-brand"></div>
+        <Link href="/#home" className="nav-brand">
+          <Image
+            alt="Logo"
+            src="/logo.png"
+            width={1500}
+            height={500}
+            style={{
+              height: "100%",
+              width: "auto",
+            }}
+          />
+        </Link>
         <div className="nav-collapse content text-bold">
           {Routes.map((item, index) => (
             <Link key={index} href={item.href}>
