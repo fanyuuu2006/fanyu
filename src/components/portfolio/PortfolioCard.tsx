d5e9556b883d7e1d039c15a56eabc9344a578aa0
Yorn90104 <bingxiao526@gmail.com>
2025-05-01 13:14:43 +0800

    refactor: 作品集卡片提出單獨組件

---
+++ src/components/portfolio/PortfolioCard.tsx
@@ -0,0 +1,111 @@
+import Image from "next/image";
+import { PortfolioItem, PortfolioLinkCategory } from "@/types/portfolio";
+import { slugify } from "@/utils/url";
+import { useLanguage } from "@/context/LanguageContext";
+import { OutsideLink } from "fanyucomponents";
+import {
+  ClockCircleOutlined,
+  CodeSandboxOutlined,
+  GithubOutlined,
+  LinkOutlined,
+  TagOutlined,
+} from "@ant-design/icons";
+
+const categoryIcon: Record<PortfolioLinkCategory, React.ReactNode> = {
+  demo: <LinkOutlined />,
+  github: <GithubOutlined />,
+  package: <CodeSandboxOutlined />,
+};
+
+export interface PortfolioCardProps
+  extends React.HTMLAttributes<HTMLDivElement> {
+  item: PortfolioItem;
+}
+
+export const PortfolioCard = ({ item, ...rest }: PortfolioCardProps) => {
+  const Language = useLanguage();
+  return (
+    <div
+      id={slugify(item.title.english)}
+      className="card bordered shadow d-flex no-wrap-on-desktop"
+      style={{
+        width: "100%",
+        padding: "1em",
+        gap: "1em",
+      }}
+      {...rest}
+    >
+      <Image
+        className="title shadow"
+        src={item.imageSrc}
+        alt={`${item.title.english} icon`}
+        width={300}
+        height={300}
+        style={{
+          height: "1.5em",
+          width: "auto",
+          borderRadius: "100%",
+        }}
+      />
+      <div className="d-flex flex-column flex-grow" style={{ gap: "0.5em" }}>
+        <div className="content text-bold">{item.title[Language.Current]}</div>
+        <div className="hint d-flex" style={{ gap: "0.5em" }}>
+          <ClockCircleOutlined />
+          {item.time}
+        </div>
+        <div className="note">{item.about[Language.Current]}</div>
+        {item.links.map((link) => (
+          <OutsideLink
+            key={link.href}
+            href={link.href}
+            className="hint d-flex"
+            style={{
+              width: "fit-content",
+              flexWrap: "nowrap",
+              gap: "0.5em",
+              opacity: "0.7",
+            }}
+          >
+            {categoryIcon[link.category]}
+            <span>{link.href}</span>
+          </OutsideLink>
+        ))}
+        <ul className="note">
+          {item.description[Language.Current].map((part, index) => (
+            <li key={index}>{part}</li>
+          ))}
+        </ul>
+        <div
+          className="hint d-flex"
+          style={{
+            flexWrap: "nowrap",
+            gap: "0.5em",
+          }}
+        >
+          <TagOutlined />
+          <div
+            className="d-flex"
+            style={{
+              flexWrap: "wrap",
+              gap: "0.5em",
+            }}
+          >
+            {item.tags.map((tag) => (
+              <span
+                key={tag}
+                style={{
+                  borderRadius: "5px",
+                  whiteSpace: "nowrap",
+                  padding: "0 0.5em",
+                  backgroundColor: "var(--background-color)",
+                }}
+              >
+                {tag}
+              </span>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/components/portfolio/PortfolioSection.tsx
+++ src/components/portfolio/PortfolioSection.tsx
@@ -1,27 +1,16 @@
 "use client";
-import Image from "next/image";
 import { useLanguage } from "@/context/LanguageContext";
 import { portfolio, portfolioTagCategories } from "@/lib/portfolio";
 import { LanguageContent, LanguageOption } from "@/types/language";
 import {
   PortfolioItem,
-  PortfolioLinkCategory,
   PortfolioTag,
   PortfolioTagCategory,
 } from "@/types/portfolio";
-import {
-  ClockCircleOutlined,
-  CodeSandboxOutlined,
-  DownOutlined,
-  GithubOutlined,
-  LinkOutlined,
-  MenuOutlined,
-  TagOutlined,
-} from "@ant-design/icons";
-import { OutsideLink } from "fanyucomponents";
+import { DownOutlined, MenuOutlined } from "@ant-design/icons";
 import Link from "next/link";
 import React, { useMemo, useRef, useState } from "react";
-import { slugify } from "@/utils/url";
+import { PortfolioCard } from "./PortfolioCard";
 
 type PortfolioContent = Record<
   "portfolio" | "nofound" | "all" | "categories" | PortfolioTagCategory,
@@ -59,12 +48,6 @@ const getPortfolioContent = (language: LanguageOption): PortfolioContent =>
       },
     } as LanguageContent<PortfolioContent>
   )[language]);
-
-const categoryIcon: Record<PortfolioLinkCategory, React.ReactNode> = {
-  demo: <LinkOutlined />,
-  github: <GithubOutlined />,
-  package: <CodeSandboxOutlined />,
-};
 
 export const PortfolioSection = () => {
   const Language = useLanguage();
@@ -173,93 +156,7 @@ export const PortfolioSection = () => {
           <>{portfolioContent.nofound}</>
         ) : (
           filteredPortfolio.map((item: PortfolioItem) => (
-            <div
-              key={item.title.english}
-              id={slugify(item.title.english)}
-              className="card bordered shadow d-flex no-wrap-on-desktop"
-              style={{
-                width: "100%",
-                padding: "1em",
-                gap: "1em",
-              }}
-            >
-              <Image
-                className="title shadow"
-                src={item.imageSrc}
-                alt={`${item.title.english} icon`}
-                width={300}
-                height={300}
-                style={{
-                  height: "1.5em",
-                  width: "auto",
-                  borderRadius: "100%",
-                }}
-              />
-              <div
-                className="d-flex flex-column flex-grow"
-                style={{ gap: "0.5em" }}
-              >
-                <div className="content text-bold">
-                  {item.title[Language.Current]}
-                </div>
-                <div className="hint d-flex" style={{ gap: "0.5em" }}>
-                  <ClockCircleOutlined />
-                  {item.time}
-                </div>
-                <div className="note">{item.about[Language.Current]}</div>
-                {item.links.map((link) => (
-                  <OutsideLink
-                    key={link.href}
-                    href={link.href}
-                    className="hint d-flex"
-                    style={{
-                      width: "fit-content",
-                      flexWrap: "nowrap",
-                      gap: "0.5em",
-                      opacity: "0.7",
-                    }}
-                  >
-                    {categoryIcon[link.category]}
-                    <span>{link.href}</span>
-                  </OutsideLink>
-                ))}
-                <ul className="note">
-                  {item.description[Language.Current].map((part, index) => (
-                    <li key={index}>{part}</li>
-                  ))}
-                </ul>
-                <div
-                  className="hint d-flex"
-                  style={{
-                    flexWrap: "nowrap",
-                    gap: "0.5em",
-                  }}
-                >
-                  <TagOutlined />
-                  <div
-                    className="d-flex"
-                    style={{
-                      flexWrap: "wrap",
-                      gap: "0.5em",
-                    }}
-                  >
-                    {item.tags.map((tag) => (
-                      <span
-                        key={tag}
-                        style={{
-                          borderRadius: "5px",
-                          whiteSpace: "nowrap",
-                          padding: "0 0.5em",
-                          backgroundColor: "var(--background-color)",
-                        }}
-                      >
-                        {tag}
-                      </span>
-                    ))}
-                  </div>
-                </div>
-              </div>
-            </div>
+            <PortfolioCard key={item.title.english} item={item} />
           ))
         )}
         <Link className="note" href="/#portfolio">
