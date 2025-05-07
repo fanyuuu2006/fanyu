3621ca9ba530b1753f9cae9ff08b5252c587b4c7
Yorn90104 <bingxiao526@gmail.com>
2025-05-08 01:21:28 +0800

    feature: github badge

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,14 +12,40 @@ const getFooterContent = (language: LanguageOption): FooterContent =>
     {
       chinese: {
         copyright: "版權所有。",
-        sourceCode: "原始碼",
+        sourceCode: "原始碼: ",
       },
       english: {
         copyright: " All rights reserved.",
-        sourceCode: "Source code",
+        sourceCode: "Source code: ",
       },
     } as LanguageContent<FooterContent>
   )[language]);
+
+const githubBadgeItems: {
+  title: string;
+  href: string;
+}[] = [
+  {
+    title: "License",
+    href: "https://img.shields.io/github/license/fanyuuu2006/fanyu?style=flat-square",
+  },
+  {
+    title: "Stars",
+    href: "https://img.shields.io/github/stars/fanyuuu2006/fanyu?style=flat-square",
+  },
+  {
+    title: "Last commit",
+    href: "https://img.shields.io/github/last-commit/fanyuuu2006/fanyu?style=flat-square",
+  },
+  {
+    title: "Created at",
+    href: "https://img.shields.io/github/created-at/fanyuuu2006/fanyu?style=flat-square",
+  },
+  {
+    title: "Repository size",
+    href: "https://img.shields.io/github/repo-size/fanyuuu2006/fanyu?style=flat-square",
+  },
+];
 
 export const Footer = () => {
   const Langauge = useLanguage();
@@ -28,24 +54,31 @@ export const Footer = () => {
   return (
     <footer>
       <div className="container flex flex-col items-center">
-        <div className="note">
-          <div className="flex gap-4">
+        <div className="hint flex flex-col gap-2">
+          <div className="flex gap-2">
             <CopyrightOutlined /> {year}, {profile.nickname[Langauge.Current]}
             {footerContent.copyright}
           </div>
-          <div className="flex items-center gap-2">
+          <div className="flex flex-wrap items-center gap-x-2">
             <CodeOutlined /> {footerContent.sourceCode}
-            <span>
-              {"["}
-              <OutsideLink href="https://github.com/fanyuuu2006/fanyu" />
-              {"]"}
-            </span>
-            {/* eslint-disable-next-line @next/next/no-img-element*/}
-            <img
-              src="https://img.shields.io/github/license/fanyuuu2006/fanyu"
-              alt="license"
-              className="h-fit"
+            <OutsideLink
+              className="whitespace-nowrap"
+              href="https://github.com/fanyuuu2006/fanyu"
             />
+          </div>
+          <div className="flex flex-wrap gap-2">
+            {githubBadgeItems.map((item) => (
+              <>
+                {/* eslint-disable-next-line @next/next/no-img-element*/}
+                <img
+                  key={item.title}
+                  src={item.href}
+                  alt={item.title}
+                  title={item.title}
+                  className="h-fit"
+                />
+              </>
+            ))}
           </div>
         </div>
       </div>
