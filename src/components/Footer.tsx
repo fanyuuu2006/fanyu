88b6dd872c49aae84f6d4199b6c0ba4c28b9865e
Yorn90104 <bingxiao526@gmail.com>
2025-05-08 00:36:06 +0800

    feature: 原始碼連結

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,18 +2,21 @@
 import { useLanguage } from "@/context/LanguageContext";
 import { profile } from "@/lib/profile";
 import { LanguageContent, LanguageOption } from "@/types/language";
-import { CopyrightOutlined } from "@ant-design/icons";
+import { CodeOutlined, CopyrightOutlined } from "@ant-design/icons";
+import { OutsideLink } from "fanyucomponents";
 
-type FooterContent = Record<"copyright", string>;
+type FooterContent = Record<"copyright" | "sourceCode", string>;
 
 const getFooterContent = (language: LanguageOption): FooterContent =>
   ((
     {
       chinese: {
         copyright: "版權所有。",
+        sourceCode: "原始碼",
       },
       english: {
         copyright: " All rights reserved.",
+        sourceCode: "Source code",
       },
     } as LanguageContent<FooterContent>
   )[language]);
@@ -26,8 +29,24 @@ export const Footer = () => {
     <footer>
       <div className="container flex flex-col items-center">
         <div className="note">
-          <CopyrightOutlined /> {year}, {profile.nickname[Langauge.Current]}{" "}
-          {footerContent.copyright}
+          <div className="flex gap-4">
+            <CopyrightOutlined /> {year}, {profile.nickname[Langauge.Current]}
+            {footerContent.copyright}
+          </div>
+          <div className="flex items-center gap-2">
+            <CodeOutlined /> {footerContent.sourceCode}
+            <span>
+              {"["}
+              <OutsideLink href="https://github.com/fanyuuu2006/fanyu" />
+              {"]"}
+            </span>
+            {/* eslint-disable-next-line @next/next/no-img-element*/}
+            <img
+              src="https://img.shields.io/github/license/fanyuuu2006/fanyu"
+              alt="license"
+              className="h-fit"
+            />
+          </div>
         </div>
       </div>
     </footer>
