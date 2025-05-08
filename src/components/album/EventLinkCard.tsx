402f8a4bc6100c696011e61f3b2c03faecce2273
Yorn90104 <bingxiao526@gmail.com>
2025-05-08 15:13:56 +0800

    refactor: 移除通用Lazy組件

--- src/components/album/EventLinkCard.tsx
+++ src/components/album/EventLinkCard.tsx
@@ -1,7 +1,7 @@
 import { slugify } from "@/utils/url";
 import { DistributiveOmit, OverrideProps } from "fanyucomponents";
 import Link from "next/link";
-import { LazyImage } from "../common/LazyImage";
+
 export type EventLinkCardProps = DistributiveOmit<
   OverrideProps<
     React.ComponentPropsWithRef<typeof Link>,
@@ -22,25 +22,22 @@ export const EventLinkCard = ({
 }: EventLinkCardProps) => {
   return (
     <Link
-      className="p-[1px] w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5 group"
-      href={`/album/${slugify(year)}/${slugify(eventName)}`}
       aria-label={`前往 ${eventName} 相簿`}
+      className="select-none p-[1px] relative overflow-hidden group w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5"
+      href={`/album/${slugify(year)}/${slugify(eventName)}`}
       {...rest}
     >
-      <LazyImage
-        containerProps={{
-          children: (
-            <span className="absolute w-full bg-[#000] opacity-50 bottom-0 hint font-bold text-center group-hover:opacity-100 transition-opacity">
-              {eventName}
-            </span>
-          ),
-        }}
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img
         draggable={false}
         loading="lazy"
         src={imageSrcs[0]}
         alt={eventName}
-        className="aspect-square w-full object-cover transition duration-300 group-hover:brightness-50"
+        className="aspect-square w-full bg-[#888] object-cover transition duration-300 group-hover:brightness-50"
       />
+      <span className="absolute w-full bg-[#000] opacity-50 bottom-0 hint font-bold text-center group-hover:opacity-100 transition-opacity">
+        {eventName}
+      </span>
     </Link>
   );
 };
--- src/components/album/[year]/[eventName]/ImageCard.tsx
+++ src/components/album/[year]/[eventName]/ImageCard.tsx
@@ -1,8 +1,9 @@
-import { LazyImage } from "@/components/common/LazyImage";
 import { Toast } from "@/components/common/Toast";
 import { useLanguage } from "@/context/LanguageContext";
 import { LanguageOption, LanguageContent } from "@/types/language";
+import { LoadingOutlined } from "@ant-design/icons";
 import { useModal } from "fanyucomponents";
+import { useState } from "react";
 
 type ImageContent = Record<"imageLoadFailed", string>;
 
@@ -24,13 +25,24 @@ export const ImageCard = ({
   const Language = useLanguage();
   const imageContent = getImageContent(Language.Current);
   const Modal = useModal();
+  const [loading, setLoading] = useState(true);
+
+  const handleError = (e: React.SyntheticEvent) => {
+    console.error(e);
+    Toast.fire({ icon: "error", text: imageContent.imageLoadFailed });
+    setLoading(false);
+  };
 
   return (
-    <>
-      <LazyImage
-        containerProps={{
-          className: "p-[1px] w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5 group",
-        }}
+    <div className="relative bg-[#888] p-[1px] w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5 group">
+      {/* Spinner */}
+      {loading && (
+        <div className="absolute inset-0 flex items-center justify-center ">
+          <LoadingOutlined className="content" />
+        </div>
+      )}
+      {/* eslint-disable-next-line @next/next/no-img-element*/}
+      <img
         draggable={false}
         src={src}
         alt={src?.toString()}
@@ -38,6 +50,10 @@ export const ImageCard = ({
         onClick={() => {
           Modal.Open();
         }}
+        onLoad={() => {
+          setLoading(false);
+        }}
+        onError={handleError}
       />
       <Modal.Container>
         {/* eslint-disable-next-line @next/next/no-img-element */}
@@ -45,12 +61,9 @@ export const ImageCard = ({
           src={src}
           alt={src?.toString()}
           className="max-w-[95vw] max-h-[80vh] object-contain"
-          onError={(e: React.SyntheticEvent) => {
-            console.error(e);
-            Toast.fire({ icon: "error", text: imageContent.imageLoadFailed });
-          }}
+          onError={handleError}
         />
       </Modal.Container>
-    </>
+    </div>
   );
 };
