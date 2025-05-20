0ef4b7e9048795d6b7ad09e141bf01d87e767100
Yorn90104 <bingxiao526@gmail.com>
2025-05-20 18:24:57 +0800

    refactor: 用 @giscus/react

--- src/components/guestbook/MainSection.tsx
+++ src/components/guestbook/MainSection.tsx
@@ -2,8 +2,7 @@
 
 import { useLanguage } from "@/context/LanguageContext";
 import { LanguageContent, LanguageOption } from "@/types/language";
-import { LoadingOutlined } from "@ant-design/icons";
-import { useEffect } from "react";
+import Giscus from "@giscus/react";
 
 type GuestbookContent = Record<"guestbook", string>;
 
@@ -23,42 +22,28 @@ export const MainSection = () => {
   const Language = useLanguage();
   const guestbookContent = getGuestbookContent(Language.Current);
 
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://giscus.app/client.js";
-    script.setAttribute("data-repo", "fanyuuu2006/fanyu");
-    script.setAttribute("data-repo-id", "R_kgDOOeftZg");
-    script.setAttribute("data-category", "Announcements");
-    script.setAttribute("data-category-id", "DIC_kwDOOeftZs4CqXwr");
-    script.setAttribute("data-mapping", "pathname");
-    script.setAttribute("data-strict", "0");
-    script.setAttribute("data-reactions-enabled", "1");
-    script.setAttribute("data-emit-metadata", "0");
-    script.setAttribute("data-input-position", "top");
-    script.setAttribute("data-theme", "preferred_color_scheme");
-    script.setAttribute(
-      "data-lang",
-      Language.Current === "chinese" ? "zh-TW" : "en"
-    );
-    script.setAttribute("data-loading", "lazy");
-    script.setAttribute("crossorigin", "anonymous");
-
-    const container = document.getElementById("guestbook-container");
-    if (container) {
-      container.innerHTML = ""; // 清空舊留言
-      container.appendChild(script);
-    }
-  }, [Language.Current]); // 語言切換時重新載入留言板
-
   return (
     <section>
       <div className="container flex flex-col items-center">
-        <div className="title font-bold">{guestbookContent.guestbook}</div>
+        <span className="title font-bold">{guestbookContent.guestbook}</span>
         <div
           className="w-full flex flex-col items-center p-4"
           id="guestbook-container"
         >
-          <LoadingOutlined className="content" />
+          <Giscus
+            repo="fanyuuu2006/fanyu"
+            repoId="R_kgDOOeftZg"
+            category="Announcements"
+            categoryId="DIC_kwDOOeftZs4CqXwr"
+            mapping="pathname"
+            strict="0"
+            reactionsEnabled="1"
+            emitMetadata="0"
+            inputPosition="top"
+            theme="preferred_color_scheme"
+            lang={Language.Current === "chinese" ? "zh-TW" : "en"}
+            loading="lazy"
+          />
         </div>
       </div>
     </section>
