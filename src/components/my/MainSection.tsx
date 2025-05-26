ff0d7433c2b0fb189e6eae48a39ca96df0eac5f1
Yorn90104 <bingxiao526@gmail.com>
2025-05-26 10:20:23 +0800

    feat: 調整MainSection元件的樣式，移除亮度設定並更新卡片邊框樣式

--- src/components/my/MainSection.tsx
+++ src/components/my/MainSection.tsx
@@ -69,7 +69,7 @@ export const MainSection = () => {
     <section id="hero">
       <div className="container flex flex-col items-center justify-center min-h-162">
         {timeLeft ? (
-          <div className="card flex flex-col gap-2 items-center px-8 py-4 brightness-[var(--brightness-light)]">
+          <div className="card flex flex-col gap-2 items-center px-8 py-4">
             <span className="title font-bold">{myContent.birthdayTimer}</span>
             <div className="flex gap-2 items-end label">
               <TimeUnit
