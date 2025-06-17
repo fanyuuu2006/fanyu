99eed1004873ec4d33649fafd83ec8b7be2287ec
Yorn90104 <bingxiao526@gmail.com>
2025-06-18 01:23:22 +0800

    fix: 更新c063套件至1.4.9並調整generateCodeLines函數以改善代碼格式

--- src/components/Index/HeroSection/codeLines.tsx
+++ src/components/Index/HeroSection/codeLines.tsx
@@ -1,6 +1,6 @@
 import { profile } from "@/libs/profile";
 import { LanguageOption } from "@/types/language";
-import c063, { CodeTokenProps, whiteSpace } from "c063";
+import c063, { CodeTokenProps } from "c063";
 import Link from "next/link";
 
 export const generateCodeLines = (
@@ -8,38 +8,28 @@ export const generateCodeLines = (
 ): CodeTokenProps<React.ElementType>[][] => {
   return [
     [
-      c063.keyword1("const"),
-      whiteSpace(1),
+      c063.keyword1("const "),
       c063.constant(profile.nickname.english),
-      whiteSpace(1),
-      c063.operator("="),
-      whiteSpace(1),
+      c063.operator(" = "),
       c063.brackets1("{"),
     ],
     [
-      whiteSpace(2),
-      c063.variable(`name:`),
-      whiteSpace(1),
+      c063.variable(`  name: `),
       c063.string(`'${profile.name[language]}'`),
       c063.default(","),
     ],
     [
-      whiteSpace(2),
-      c063.variable(`nickname:`),
-      whiteSpace(1),
+      c063.variable(`  nickname: `),
       c063.string(`'${profile.nickname[language]}'`),
       c063.default(","),
     ],
     [
-      whiteSpace(2),
-      c063.variable(`age:`),
-      whiteSpace(1),
+      c063.variable(`  age: `),
       c063.number(`${profile.age()}`, {
         as: Link,
         href: "/my",
       }),
-      c063.default(","),
-      whiteSpace(1),
+      c063.default(", "),
       c063.comment(
         `// <-${
           {
@@ -50,9 +40,7 @@ export const generateCodeLines = (
       ),
     ],
     [
-      whiteSpace(2),
-      c063.variable(`hobbies:`),
-      whiteSpace(1),
+      c063.variable(`  hobbies: `),
       c063.brackets2("["),
       c063.string(
         `'${
@@ -62,8 +50,7 @@ export const generateCodeLines = (
           }[language]
         }'`
       ),
-      c063.default(","),
-      whiteSpace(1),
+      c063.default(", "),
       c063.string(
         `'${
           {
@@ -76,31 +63,24 @@ export const generateCodeLines = (
       c063.default(","),
     ],
     [
-      whiteSpace(2),
-      c063.variable(`skills:`),
-      whiteSpace(1),
+      c063.variable(`  skills: `),
       c063.brackets2("["),
       c063.string(`'TypeScript'`),
-      c063.default(","),
-      whiteSpace(1),
+      c063.default(", "),
       c063.string(`'React'`),
-      c063.default(","),
-      whiteSpace(1),
+      c063.default(", "),
       c063.string(`'Python'`),
       c063.brackets2("]"),
       c063.default(","),
     ],
     [
-      c063.brackets1("}"),
-      whiteSpace(1),
-      c063.keyword2("as"),
-      whiteSpace(1),
+      c063.brackets1("} "),
+      c063.keyword2("as "),
       c063.type("const"),
       c063.default(";"),
     ],
     [
-      c063.keyword2("export default"),
-      whiteSpace(1),
+      c063.keyword2("export default "),
       c063.constant("FanYu"),
       c063.default(";"),
     ],
