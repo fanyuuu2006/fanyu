f7f27ced5afa189e3fa56bd9fa76ae81754a3c53
Yorn90104 <bingxiao526@gmail.com>
2025-06-21 00:01:37 +0800

    fix: 調整ContactCard中的頭像顯示，新增邊框樣式以增強可視性；優化名稱和ID的顯示結構

--- src/components/Index/ContactSection/ContactCard.tsx
+++ src/components/Index/ContactSection/ContactCard.tsx
@@ -50,43 +50,49 @@ export const ContactCard = ({ item, ...rest }: ContactCardProps) => {
               : "var(--text-color-primary)",
           }}
         >
-          <div className="bg-[var(--background-color-dark)] rounded-[inherit] p-4">
-            <div className="flex flex-col gap-2">
-              <div className="flex items-center gap-4">
-                {/**頭像 */}
-                <div className="h-10 aspect-square rounded-full">
-                  {/*eslint-disable-next-line @next/next/no-img-element */}
-                  <img
-                    className="bg-white w-full h-full object-cover rounded-full "
-                    src={item.info.image || `/favicon.ico`}
-                    alt={`${item.label}-${item.info.id}`}
-                  />
-                </div>
+          <div className="bg-[var(--background-color-dark)] rounded-[inherit] flex flex-col gap-2 p-4">
+            <div className="flex items-center gap-4">
+              {/**頭像 */}
+              <div
+                className="h-10 aspect-square rounded-full overflow-hidden"
+                style={{
+                  border: `2px solid ${
+                    item.backgrounds?.[0] || "var(--text-color-primary)"
+                  }`,
+                }}
+              >
+                {/*eslint-disable-next-line @next/next/no-img-element */}
+                <img
+                  className="bg-white w-full h-full object-cover"
+                  src={item.info.image || `/favicon.ico`}
+                  alt={`${item.label}-${item.info.id}`}
+                />
+              </div>
 
-                {/**名稱 & ID */}
-                <div className="flex flex-col gap-0 whitespace-nowrap">
-                  <span className="note font-bold">{item.info.name}</span>
-                  <span className="hint flex items-center gap-1">
-                    {item.info.id}
-                    <CopyButton content={item.info.id} />
-                  </span>
-                </div>
-              </div>
-              {/**相關資訊(?) */}
-              <div className="flex">
-                <OutsideLink
-                  className="ms-auto note flex gap-2"
-                  href={item.href}
+              {/**名稱 & ID */}
+              <div className="flex flex-col gap-0 whitespace-nowrap">
+                <span
+                  className="note font-bold"
                 >
-                  {
-                    {
-                      chinese: "前往",
-                      english: "Go to",
-                    }[Language.Current]
-                  }
-                  <ArrowRightOutlined/>
-                </OutsideLink>
+                  {item.info.name}
+                </span>
+                <span className="hint flex items-center gap-1">
+                  {item.info.id}
+                  <CopyButton content={item.info.id} />
+                </span>
               </div>
+            </div>
+            {/**相關資訊(?) */}
+            <div className="flex">
+              <OutsideLink className="ms-auto note flex gap-2" href={item.href}>
+                {
+                  {
+                    chinese: "前往",
+                    english: "Go to",
+                  }[Language.Current]
+                }
+                <ArrowRightOutlined />
+              </OutsideLink>
             </div>
           </div>
         </div>
