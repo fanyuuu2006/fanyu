c14aee8fc01806d36fc2351344175377adf67005
Yorn90104 <bingxiao526@gmail.com>
2025-07-07 13:52:16 +0800

    fix: 移除slugify和deslugify函數中的自定義分隔符處理，恢復為基本的編碼和解碼功能

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,4 +1,2 @@
-export const slugify = (s: string) =>
-  encodeURIComponent(s.replace(/\s+/g, "__FANYU__"));
-export const deslugify = (s: string) =>
-  decodeURIComponent(s.replace(/__FANYU__/g, " "));
+export const slugify = (s: string) => encodeURIComponent(s);
+export const deslugify = (s: string) => decodeURIComponent(s);
