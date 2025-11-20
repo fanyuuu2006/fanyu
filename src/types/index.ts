b417f2eaf749e15172c942fef38767cb3fcb9e8d
Yorn90104 <bingxiao526@gmail.com>
2025-11-20 09:30:31 +0800

    feat: 更新年份類型定義，簡化邏輯並強化日期驗證功能

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,10 +1,26 @@
+export type ReversedString<S extends string> =
+  S extends `${infer First}${infer Rest}`
+    ? `${ReversedString<Rest>}${First}`
+    : S;
+
 // 單位數字
 type Digit = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
 
-// 年份：1900–2099
-type Year =
-  | `19${Digit}${Digit}` // 1900–1999
-  | `20${Digit}${Digit}`; // 2000–2099
+// type Year = `20${Digit}${Digit}`;
+type Year = `${number}`;
+
+// type IsLeapYear<Y extends Year> =
+//   ReversedString<Y> extends `${infer Ones}${infer Tens}${number}`
+//     ? Tens extends "0" | "2" | "4" | "6" | "8"
+//       ? Ones extends "0" | "4" | "8"
+//         ? true
+//         : false
+//       : Tens extends "1" | "3" | "5" | "7" | "9"
+//       ? Ones extends "2" | "6"
+//         ? true
+//         : false
+//       : false
+//     : false;
 
 // 月份：01–12
 type Month =
@@ -19,30 +35,28 @@ type Day =
   | `3${0 | 1}`; // 30–31
 
 type IsValidDate<M extends Month, D extends Day> = M extends "02"
-  ? D extends Exclude<Day, "30" | "31">
-    ? D
-    : never
+  ? D extends Exclude<Day, "29" | "30" | "31">
+    ? true
+    : false
   : M extends "04" | "06" | "09" | "11"
   ? D extends Exclude<Day, "31">
-    ? D
-    : never
-  : D;
+    ? true
+    : false
+  : true;
 
 type YYMM = `${Year}-${Month}`;
 type YYMMDD =
   `${Year}-${Month}-${Day}` extends `${infer Y}-${infer M}-${infer D}`
-    ? M extends Month
-      ? D extends Day
-        ? IsValidDate<M, D> extends never
-          ? never
-          : `${Y}-${M}-${D}`
+    ? Y extends Year
+      ? M extends Month
+        ? D extends Day
+          ? IsValidDate<M, D> extends true
+            ? `${Y}-${M}-${D}`
+            : never
+          : never
         : never
       : never
     : never;
 
 // 嚴謹的日期字符串類型
 export type DateString = YYMM | YYMMDD;
-
-export const t1: DateString = "2024-06";
-export const t2: DateString = "2023-02-29"; // 錯誤，2023 不是閏年
-export const t3: DateString = "2020-02-29";
