347ee09bd5effe78e44d012c9d406c6ea1ecca96
Yorn90104 <bingxiao526@gmail.com>
2025-12-15 23:10:38 +0800

    fix: 將 FALLBACK_IMAGE 的 URL 包裝為 proxyUrl，以提升圖片加載的靈活性

--- src/libs/album.tsx
+++ src/libs/album.tsx
@@ -1,2 +1,4 @@
+import { proxyUrl } from "@/utils/url";
+
 export const FALLBACK_IMAGE =
-  "https://www.kleinpaint.ca/cdn/shop/products/444A50.png?v=1666632521&width=713";
+  proxyUrl("https://www.kleinpaint.ca/cdn/shop/products/444A50.png?v=1666632521&width=713");
