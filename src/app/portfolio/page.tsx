bd0ce2c8796b26641d8157534c37716740a2bcf8
Yorn90104 <bingxiao526@gmail.com>
2026-07-12 02:39:59 +0800

    fix: useSearchParams() 時，必須被 <Suspense>

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,5 +1,6 @@
 import { HeadingSection } from "@/components/HeadingSection";
 import { PortfolioExplorer } from "@/components/portfolio/PortfolioExplorer";
+import { Suspense } from "react";
 
 export default function Portfolio() {
   return (
@@ -8,7 +9,9 @@ export default function Portfolio() {
         title="作品集"
         description="展示飯魚（范余振富）參與開發的專案與作品。"
       />
-      <PortfolioExplorer />
+      <Suspense>
+        <PortfolioExplorer />
+      </Suspense>
     </>
   );
 }
